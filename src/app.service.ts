389e3278d4de0d83450cd8c5ff264aaec238c857
igornester <inesterenko@nucleushealth.io>
2019-12-27 17:21:30 +0200

    - add few other tasks

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  /**
+   * @todo #1:30m/DEV Get List of notification from Github
+   *  use graphql to request for notifications
+   *  rename to notification service
+   */
   getHello(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * @todo #1:30m/DEV Provide env configuration
+   *  move PORT to env variablese
+   *  use dotenv-safe approach
+   */
   await app.listen(3000);
 }
 bootstrap();
