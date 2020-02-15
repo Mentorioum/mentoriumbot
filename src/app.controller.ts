92543ac92d89da73df265707423206977929d83d
nesterone <igornester@gmail.com>
2020-02-15 12:10:46 +0200

    mentoriumbot-17 notification.service.ts:66-67: Remove notification after... - added watcher - add start/stop entry points for watcher - add interval config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { NotificationWatcher } from './NotificationWatcher';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: NotificationService) {}
+  constructor(private readonly appService: NotificationService,
+              private readonly wather: NotificationWatcher) {}
 
   @Get('searchAndMarksAsRead')
   async searchAndMarksAsRead(): Promise<object> {
     return await this.appService.searchAndReadyNotificationIfAny();
+  }
+
+  @Get('startWatcher')
+  async startWatcher(): Promise<any> {
+    return await this.wather.start(process.env.NOTIFICATION_WAKEUP_INTERVAL);
+  }
+
+  @Get('stopWatcher')
+  async stopWatcher(): Promise<any> {
+    return await this.wather.stop();
   }
 }
